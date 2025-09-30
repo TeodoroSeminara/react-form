6d2169158e38e10867508f42bf0830b57eceb1ba
Teo <seminara.teodoro93@gmail.com>
2025-09-30 18:19:15 +0200

    esercizio fatto

--- src/components/Main/ArticleList.jsx
+++ src/components/Main/ArticleList.jsx
@@ -1,24 +1,28 @@
-const articleList = [
-    {
-        "id": 1,
-        "title": "Panda ruba pranzo al guardiano dello zoo e pretende dessert"
-    },
-    {
-        "id": 2,
-        "title": "Nonna di 82 anni diventa campionessa di eSports per sbaglio"
-    },
-    {
-        "id": 3,
-        "title": "Sindaco inaugura fontana ma cade dentro: applausi del pubblico"
-    },
-    {
-        "id": 4,
-        "title": "Scoperto gatto che prende ogni giorno l’autobus per andare al parco"
-    },
-    {
-        "id": 5,
-        "title": "Uomo tenta di cucinare pasta senza acqua: vigili del fuoco intervengono"
-    }
-]
+const articleList = [{
+    "id": 1,
+    "title": "Panda ruba pranzo al guardiano dello zoo e pretende dessert"
+},
+{
+    "id": 2,
+    "title": "Nonna di 82 anni diventa campionessa di eSports per sbaglio"
+},
+{
+    "id": 3,
+    "title": "Sindaco inaugura fontana ma cade dentro: applausi del pubblico"
+},
+{
+    "id": 4,
+    "title": "Scoperto gatto che prende ogni giorno l’autobus per andare al parco"
+},
+{
+    "id": 5,
+    "title": "Uomo tenta di cucinare pasta senza acqua: vigili del fuoco intervengono"
+}]
 
 export default articleList
+
+
+
+/*"Panda ruba pranzo al guardiano dello zoo e pretende dessert", "Nonna di 82 anni diventa campionessa di eSports per sbaglio", "Sindaco inaugura fontana ma cade dentro: applausi del pubblico", "Scoperto gatto che prende ogni giorno l’autobus per andare al parco","Uomo tenta di cucinare pasta senza acqua: vigili del fuoco intervengono"
+   
+        */
--- src/components/Main/MyMain.jsx
+++ src/components/Main/MyMain.jsx
@@ -10,14 +10,14 @@ function MyMain() {
         e.preventDefault();
 
         const noSpace = newArticle.trim();
-        const updateArticle = [...article, noSpace];
-        setNewArticle(updateArticle);
-        setNewArticle(" ");
+        const updateArticle = [...article, { id: article.length + 1, title: noSpace }];
+        setArticle(updateArticle);
+        setNewArticle("");
     }
 
     const removeArticle = i => {
-        const removeArt = article.filter((art, articleIndex) => {
-            return articleIndex !== i
+        const removeArt = article.filter((art) => {
+            return art.id !== i
         });
         setArticle(removeArt);
     }
@@ -30,9 +30,9 @@ function MyMain() {
                     placeholder="Inserisci il titolo dell'articolo"
                     value={newArticle}
                     required
-                    onChange={(e) => {setNewArticle(e.target.value) }}
+                    onChange={(e) => { setNewArticle(e.target.value) }}
                 />
-                <button type="submit">Cancella</button>
+                <button type="submit">Aggiungi</button>
             </form>
             <ul>
                 {article.map((article) => (
@@ -40,7 +40,7 @@ function MyMain() {
                         key={article.id}>
                         {article.title}
                         <button onClick={() => removeArticle(article.id)}>
-                            Elimina
+                            cancella
                         </button>
                     </li>
                 ))}
